976a132d112c643bda9da119a18c94e6578ab462
Constantine <constantine.krotcov@teamaol.com>
2013-08-20 13:54:01 -0700

    getting the flight status

--- status.py
+++ status.py
@@ -11,7 +11,9 @@ class DataServer(db.Model):
 class FlightData(db.Model):
 	callsign = db.StringProperty(required=True)
 	pilot_id = db.StringProperty(required=True)
-	pilot_name = db.StringProperty(required=True)
+	pilot_name = db.StringProperty(required=False)
+	client_type = db.StringProperty(required=False)
+	frequency = db.StringProperty(required=False)
 	lat = db.StringProperty()
 	lon = db.StringProperty()
 	alt = db.StringProperty()
@@ -47,27 +49,43 @@ class GetPilotData(webapp2.RequestHandler):
 		offset = random.randrange(0, url_count)
 		url_object = DataServer.all().fetch(1, offset)[0]
 		
+		
 		try:
 			result = urllib2.urlopen(url_object.url)
 			data = result.read()
-			r1 = re.compile('((AAL|UAL|WWA).*)')
-			for item in r1.findall(str(data)):	
-				
-				pilot_data = item[0].split(':');
-				
-				self.response.write(pilot_data)
-				
-				flight_data = FlightData(callsign=pilot_data[0],
-										pilot_id=pilot_data[1],
-										pilot_name=pilot_data[2],
-										lat=pilot_data[5],
-										lon=pilot_data[6],
-										alt=pilot_data[7])
-				flight_data.put()
-				
-				self.response.write(flight_data)			
 			
 			
+			out = data.split("!CLIENTS:\r\n")
+			data_lines = out[1].split("!SERVERS:\r\n")
+			app_data = data_lines[0]
+			app_data= app_data.replace(';\r\n', "").splitlines()
+			
+			for item in app_data:
+				pilot_data = item.split(':');
+				print pilot_data			
+				if pilot_data:
+				
+					if pilot_data[1] and pilot_data[2]:
+						
+						q = db.Query(FlightData)
+						flight_data = q.filter('callsign =', pilot_data[0]).get()
+						
+						
+											
+						if not flight_data:
+							flight_data = FlightData(callsign=pilot_data[0], pilot_id=pilot_data[1])
+							
+						flight_data.callsign=pilot_data[0]
+						flight_data.pilot_id=pilot_data[1]
+						flight_data.pilot_name=pilot_data[2]
+						flight_data.client_type=pilot_data[3]
+						flight_data.frequency=pilot_data[4]
+						flight_data.lat=pilot_data[5]
+						flight_data.lon=pilot_data[6]
+						flight_data.alt=pilot_data[7]
+						flight_data.put()
+
+					
 				
 		except urllib2.URLError, e:
 			self.response.write("Error")
