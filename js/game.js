b9ad3f61d74ffa6692b31d79d4dd07545d96f130
Constantine <constantine.krotcov@teamaol.com>
2013-08-22 19:04:07 -0700

    sdfsf

--- js/game.js
+++ js/game.js
@@ -9,56 +9,79 @@ var Game = {
 	    canvas: c,
 	    debug: true
 	  });
-	  scene = this.engine.createScene();
 	  
-	  var background = new Joy.Rect({
-		  position: scene.viewport.position,
-		  color: '#62bee0',
-		  width: this.engine.width,
-		  height: this.engine.height
-		});
-	  
-	  
-	  background.bind(Joy.Events.CLICK, function(e) {
-	  	  var that = this;
-	  	  this.locked = true;
-		  var ballPosition = Game.ball.position.clone();
-		  
-		  //console.log(ballPosition);
-		  var direction = ballPosition.subtract(new Joy.Vector2d(e.x, e.y)).normalize();
-		  console.log(direction);
-		  
-		  /*
-		  new Joy.Tween(Game.ball.position).to({
-			x: e.x,
-			y: e.y  
-		  }).easing(Joy.TweenManager.Easing.Sinusoidal.InOut).
-		  	start().onComplete(function(){});
-		  */
-		});
-		
-		
-	  
-	  scene.addChild(background);
-	  
-	  var spriteUrl = "/assets/ball.png";
-	  
-	  this.ball = new Joy.Sprite({
-	  	 	x: this.engine.width/2,
-	  	 	y: this.engine.height/2, 
-	  	 	src: spriteUrl})
-	  	 	.bind('load', function() {
-	            // Change pivot to center
-	            this.pivot.x = (this.width / 2);
-	            this.pivot.y = (this.height / 2);
-			}).bind(Joy.Events.UPDATE, function(e){
-				
-				this.rotate(this.rotation+1);
-				
+	  this.engine.createScene(function(scene){
+		  Joy.Behaviour.define('BouncyBehaviour', {
+			    UPDATE: function () {
+			      this.position.x += this.speed * this.hDir * Joy.deltaTime;
+			      this.position.y += this.speed * this.vDir * Joy.deltaTime;
+			      
+			      if ((this.position.x + this.width) > this.width) {
+			        this.hDir = this.hDir*(-1);
+			      } else if (this.position.x < 0) {
+			        this.hDir = this.hDir*1;
+			      }
+			
+			      if ((this.position.y + this.height) > this.height) {
+			        this.vDir = this.vDir*(-1);
+			      } else if (this.position.y < 0) {
+			        this.vDir = this.vDir;
+			      }
+			    }
 			});
-	  scene.addChild(this.ball);
-  }
+		  
+		  var background = new Joy.Rect({
+		  		position: scene.viewport.position,
+		  		color: '#62bee0',
+		  		width: Game.engine.width,
+		  		height: Game.engine.height
+		  });
+	  
+	  
+		  background.bind(Joy.Events.CLICK, function(e) {
+		  	  var that = this;
+		  	  this.locked = true;
+			  var ballPosition = scene.ball.position.clone();
+			  
+			  //console.log(ballPosition);
+			  var direction = ballPosition.subtract(new Joy.Vector2d(e.x, e.y)).normalize();
+			  scene.ball.hDir = direction.x;
+			  scene.ball.vDir = direction.y;
+			  scene.ball.speed = 2
+			  scene.ball.behave('BouncyBehaviour');
+			  			  /*
+			  new Joy.Tween(Game.ball.position).to({
+				x: e.x,
+				y: e.y  
+			  }).easing(Joy.TweenManager.Easing.Sinusoidal.InOut).
+			  	start().onComplete(function(){});
+			  */
+			});
+		
+		
+	  
+			scene.addChild(background);
+	  
+			var spriteUrl = "/assets/ball.png";
+			
+			scene.ball = new Joy.Sprite({
+				 	x: this.width/2,
+				 	y: this.height/2, 
+				 	src: spriteUrl})
+				 	.bind('load', function() {
+			        // Change pivot to center
+			        this.pivot.x = (this.width / 2);
+			        this.pivot.y = (this.height / 2);
+				});
+			scene.addChild(scene.ball);
+		   
+	  });
+	  
+	  
+  },
+ 
 };
+
 
 
 Joy.Behaviour.define('GameMouseBehaviour', {
